d0418aeef30c71db87a9db9d45cc5d084eabb0d9
Jorge <jorge.rincon@ito-software.com>
2022-07-31 13:51:25 -0500

    Commit inicial

--- index.js
+++ index.js
@@ -25,8 +25,28 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get('/api',(req,res)=>{
+    var currentDate = new Date();
+    res.json({unix: currentDate.valueOf(),utc:currentDate.toUTCString()});
+})
+
+app.get('/api/:value',(req,res)=>{
+  var dateInput = req.params.value
+  if(/\d{5,}/.test(dateInput)){
+    var dateInt = parseInt(dateInput);
+    res.json({unix: parseInt(dateInput), utc: new Date(dateInt).toUTCString()});
+  } else {
+    var dateObject = new Date(dateInput);
+
+    if(dateObject.toString() === "Invalid Date"){
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
 });
